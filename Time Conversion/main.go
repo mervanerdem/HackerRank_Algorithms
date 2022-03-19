c4874910840c481bc0a3dda3243cce84d88fbcbe
Mervan <38705565+mervanerdem@users.noreply.github.com>
2022-03-19 16:39:44 +0300

    Solved.

--- Time Conversion/main.go
+++ Time Conversion/main.go
@@ -7,11 +7,21 @@ import (
 )
 
 func main() {
-	s := "07:05:45PM"
+	s := "02:05:45AM"
 
 	a := len(s)
 	b := string(s[a-2])
-	if b == "P" {
+
+	if string(s[0]) == "1" && string(s[1]) == "2" {
+		if b == "P" {
+			s = strings.Replace(s, "PM", "", -1)
+		} else if b == "A" {
+			s = strings.Replace(s, "12", "00", -1)
+			s = strings.Replace(s, "AM", "", -1)
+		} else {
+			fmt.Println("ERROR")
+		}
+	} else if b == "P" {
 		b := int(s[0]) - 48
 		c := int(s[1]) - 48
 		d := b*10 + c + 12
@@ -27,6 +37,8 @@ func main() {
 		s = strings.Replace(s, y, str, 1)
 
 		s = strings.Replace(s, "PM", "", -1)
+	} else {
+		s = strings.Replace(s, "AM", "", -1)
 	}
 	fmt.Println(s)
 }
